90e755f1829a3dcbc269d664b4696cbf54935fce
HalxDocs <kamsyejindu@gmail.com>
2026-05-09 14:31:31 +0100

    implement main loop for expense tracking functionality

--- main.go
+++ main.go
@@ -13,5 +13,32 @@ func main() {
 
 	expenses := []Expense{}
 
-	
+	for {
+
+		fmt.Println("==== Expense Tracker ====")
+		fmt.Println("Add Expense")
+		fmt.Println("View Expense")
+		fmt.Println("Total Expense")
+		fmt.Println("Exit")
+
+		var choice int
+		fmt.Print("Choose option:")
+		fmt.Scanln(&choice)
+
+		fmt.Println(choice)
+
+		if choice == 1 {
+	fmt.Println("Add Expense")
+} else if choice == 2 {
+	fmt.Println("View Expenses")
+} else if choice == 3 {
+	fmt.Println("Total Expenses")
+} else if choice == 4 {
+	fmt.Println("Goodbye!")
+	break
+} else {
+	fmt.Println("Invalid choice")
+}
+
+	}
 }
